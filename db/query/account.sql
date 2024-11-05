6da771b2e43bccfd180cfbca7232ccb770935bfd
lemon14454 <lemon14454@gmail.com>
2024-11-05 23:21:57 +0800

    initialize project (sqlc, go-migration, Makefile)

---
+++ db/query/account.sql
@@ -0,0 +1,40 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+  owner,
+  balance,
+  currency
+) VALUES (
+  $1, $2, $3
+)
+RETURNING *;
+
+-- name: GetAccount :one
+select * from accounts
+where id = $1 limit 1;
+
+-- name: GetAccountForUpdate :one
+select * from accounts
+where id = $1 limit 1
+FOR NO KEY UPDATE;
+
+-- name: ListAccounts :many
+select * from accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :one
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
